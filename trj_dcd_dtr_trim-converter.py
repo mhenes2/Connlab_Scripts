675b902edc301ceef0c15f05c0ccacc038b5af1a
mhenes2 <141076457+mhenes2@users.noreply.github.com>
2023-10-10 09:33:18 -0400

    can now output individual trjs in dtr and dcd formats

---
+++ trj_dcd_dtr_trim-converter.py
@@ -0,0 +1,106 @@
+import mdtraj as md
+import argparse
+from datetime import datetime
+import os
+
+startTime = datetime.now()
+
+
+def get_parser():
+    script_desc = ""
+
+    parser = argparse.ArgumentParser(description=script_desc)
+
+    parser.add_argument('infiles',
+                        help='desmond trajectory directories to be used in RMSD and protein/ligand RMSF calculations',
+                        nargs='+')
+    parser.add_argument('-top_file',
+                        help='Topology file in PDB form',
+                        type=str,
+                        required=True)
+    parser.add_argument('-outname',
+                        help='name of the job',
+                        type=str,
+                        required=True)
+    parser.add_argument('-c',
+                        help='concat trjs',
+                        type=bool,
+                        default=False)
+    parser.add_argument('-extract_asl',
+                        help='',
+                        type=str,
+                        default='protein',
+                        required=False)
+    parser.add_argument('-f',
+                        help='output format. options are "dtr", "dcd", or "both" ',
+                        type=str,
+                        default="both")
+    return parser
+
+
+def main(args):
+    topology = md.load(args.top_file).topology
+
+    if args.c != True:
+        if args.f == "dtr":
+            for index, trajectory in enumerate(args.infiles):
+                traj = md.load_dtr(trajectory, top=topology, atom_indices=topology.select(args.extract_asl))
+                print(traj)
+
+                print ('writing trj in dtr format')
+
+                traj.save_dtr("{}_rep{}.dtr".format(args.outname, index + 1))
+                print('Done saving dtr trajectory')
+
+                # rename the output file to have _trj to match Desmond output
+                os.rename("{}_rep{}.dtr".format(args.outname, index + 1), '{}_rep{}_trj'.format(args.outname, index + 1))
+
+        elif args.f == "dcd":
+            for index, trajectory in enumerate(args.infiles):
+                traj = md.load_dtr(trajectory, top=topology, atom_indices=topology.select(args.extract_asl))
+                print(traj)
+
+                print ('writing trj in dcd format')
+
+                traj.save_dcd("{}_rep{}.dcd".format(args.outname, index + 1))
+                print('Done saving dcd trajectory')
+
+        elif args.f == "both":
+            for index, trajectory in enumerate(args.infiles):
+                traj = md.load_dtr(trajectory, top=topology, atom_indices=topology.select(args.extract_asl))
+                print(traj)
+
+                print ('writing trj in dcd and dtr format')
+
+                traj.save_dcd("{}_rep{}.dcd".format(args.outname, index + 1))
+                print('Done saving dcd trajectory')
+
+                traj.save_dtr("{}_rep{}.dtr".format(args.outname, index + 1))
+                print('Done saving dtr trajectory')
+
+                # rename the output file to have _trj to match Desmond output
+                os.rename("{}_rep{}.dtr".format(args.outname, index + 1), '{}_rep{}_trj'.format(args.outname, index + 1))
+
+    #TODO should be able to do this in addition to individual trjs
+    else:
+        trj_list = []
+        for i in args.infiles:
+            trj_list.append(md.load(i))
+
+        concat_trj = md.join(trj_list)
+
+        # if concat, then only write out a dcd file
+        concat_trj.save_dcd("{}_concat_nowater".format(args.outname))
+
+        # rename the output file to have _trj to match Desmond output
+        os.rename('{}_concat_nowater.dtr'.format(args.outname), '{}_concat_nowater_trj'.format(args.outname))
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    try:
+        args = parser.parse_args()
+    except IOError as msg:
+        parser.error(str(msg))
+    main(args)
+    print(datetime.now() - startTime)
