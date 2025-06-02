3ec94e262019aeac31823938f16ac2a44f06e5fd
mhenes2 <mina.henes@emory.edu>
2025-06-02 10:48:57 -0400

    strip Desmond trj of any unwanted atoms. meant to make anaylysis easier and faster given the smaller size

--- testing-trj_desmond_extract_subsystem.py
+++ testing-trj_desmond_extract_subsystem.py
@@ -5,6 +5,10 @@ import numpy as np
 from schrodinger import structure
 from datetime import datetime  # To measure runtime duration
 import logging   # For logging progress and info
+import argparse  # For parsing command-line options
+import gc
+import os
+import shutil
 
 
 # Configure logging to output timestamps and log levels
@@ -21,7 +25,7 @@ def parse_args():
     parser.add_argument('infiles', nargs='+',
                         help='Desmond trajectory directories')
     # Required path to CMS model
-    parser.add_argument('-cms', dest='cms_file', required=True,
+    parser.add_argument('-cms', required=True,
                         help='Path to the Desmond -out.cms file')
     parser.add_argument('--name', type=str, default='nowater',
                         help='Prefix for output files')
@@ -29,6 +33,8 @@ def parse_args():
                         help='asl of atoms to extract')
     parser.add_argument('--chunks', type=int, default=5,
                         help='Number of chunks to break trajectory into')
+    parser.add_argument('--concat', action='store_true', dest='do_concat',
+                        help='Concatenate the stripped trj')
     return parser.parse_args()
 
 
@@ -45,25 +51,11 @@ def read_models(cms_path):
     Load MSYS and CMS models for reference.
     """
     # Parse CMS file to get MSYS and CMS models
-    msys_model, cms_model = topo.read_cms(str(cms_path))
+    msys_model, cms_model = topo.read_cms(cms_path)
     return msys_model, cms_model
 
 
 def new_cms(asl, output_name, cms_model, index):
-    """
-    Output a new cmsfile of the stripped system
-
-    Parameters
-    ----------
-    asl
-    output_name
-    cms_model
-    index
-
-    Returns
-    -------
-
-    """
     stripped_system = topo.extract_subsystem(cms_model, asl)
     cms.Cms.write(stripped_system[0], '{}_stripped_rep{}-out.cms'.format(output_name, index))
 
@@ -74,17 +66,20 @@ def split(list_a, chunk_size):
 
 
 def extract_system(output_name, trj_in, chunks, index):
-    sub_msys_model, sub_cms_model = topo.read_cms('{}_stripped-out.cms'.format(output_name))
+    sub_msys_model, sub_cms_model = topo.read_cms('{}_stripped_rep{}-out.cms'.format(output_name, index))
 
     trj_out = traj.read_traj(trj_in)
 
     trj = list(split(trj_out, 4002))
 
+    intermediates = []
     for i in range(chunks):
         extract = topo.asl2gids(sub_cms_model, 'all', include_pseudoatoms=True)
         traj.write_traj(traj.extract_subsystem(trj[i], extract), fname='{}_rep{}_{}_trj'
                         .format(output_name, index, i))
+        intermediates.append(str('{}_rep{}_{}_trj'.format(output_name, index, i)))
 
+    return intermediates
 
 def concatenate_lists(list_of_lists):
     concatenated_list = []
@@ -100,7 +95,19 @@ def concatenate_trj(chunks, output_name, index):
         trj_out = traj.read_traj(trj_in)
         x.append(trj_out)
 
-    traj.write_traj(traj.merge(concatenate_lists(x)), fname='{}_stripped_trj'.format(output_name))
+    traj.write_traj(traj.merge(concatenate_lists(x)), fname='{}_stripped_rep{}_trj'.format(output_name, index))
+
+
+def del_tmpfiles(dirs):
+    for d in dirs:
+        if os.path.isdir(d):
+            try:
+                shutil.rmtree(d)
+                print(f"Deleted directory {d!r}")
+            except Exception as e:
+                print(f"Error deleting {d!r}: {e}")
+        else:
+            print(f"Not a directory or doesn’t exist: {d!r}")
 
 
 def main():
@@ -114,12 +121,31 @@ def main():
     msys_model, cms_model = read_models(args.cms)
 
     for idx, trj_dir in enumerate(args.infiles, start=1):
+        tmpfiles = []
         # write out the new cms file first
-        new_cms(args.asl, args.name, msys_model, idx)
+        new_cms(args.asl, args.name, cms_model, idx)
 
-        extract_system(args.name, trj_dir, args.chunks, idx)
+        tmpfiles.append(extract_system(args.name, trj_dir, args.chunks, idx))
 
         concatenate_trj(args.chunks, args.name, idx)
+
+    # if args.do_concat:
+    #     trajs = []
+    #     for f in args.infiles:
+    #         tr = traj.read_traj(f)
+    #         trajs.append(tr)
+    #     result = list(traj.concat(*trajs))
+    #     print('The resultant trajectory has {} frames.'.format(len(result)))
+    #     traj.write_traj(result, fname='{}_stripped_concat_trj'.format(args.name))
+    #     out_cms_fname = args.output_name + 'concat-out.cms'
+    #     cms_model.fix_filenames(out_cms_fname, '{}_stripped_concat_trj'.format(args.name))
+    #     cms_model.write(out_cms_fname)
+
+    # flatten a list of lists
+    tmpfiles = [item for sublist in tmpfiles for item in sublist]
+
+    #cleanup
+    del_tmpfiles(tmpfiles)
 
     # Log total runtime
     duration = datetime.now() - start_time
